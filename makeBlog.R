0da4bac7afc835f127be605bdbe0eb423358390d
jtipton25 <jtipton25@gmail.com>
2016-01-07 14:47:48 -0700

    test

--- makeBlog.R
+++ makeBlog.R
@@ -1,3 +1,4 @@
 library(servr)
-# jekyll(input="_drafts/", output="_posts/")
+# jekyll()
+# jekyll(command="bundle exec jekyll build")
 jekyll(dir=".", input="_drafts", output="_posts", command="bundle exec jekyll build")
